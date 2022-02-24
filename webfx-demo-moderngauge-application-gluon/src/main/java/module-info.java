8a83706cc7df4eb6e672995832cdc24d1da3b2ac
Bruno Salmon <dev.salmonb@gmail.com>
2022-02-24 20:17:33 +0000

    Renamed appcontainer to boot in module names and java code

--- webfx-demo-moderngauge-application-gluon/src/main/java/module-info.java
+++ webfx-demo-moderngauge-application-gluon/src/main/java/module-info.java
@@ -5,7 +5,7 @@ module webfx.demo.moderngauge.application.gluon {
     // Direct dependencies modules
     requires webfx.demo.moderngauge.application;
     requires webfx.kit.openjfx;
-    requires webfx.platform.java.appcontainer.impl;
+    requires webfx.platform.java.boot.impl;
     requires webfx.platform.java.scheduler.impl;
     requires webfx.platform.java.shutdown.impl;
     requires webfx.platform.shared.log.impl.simple;
